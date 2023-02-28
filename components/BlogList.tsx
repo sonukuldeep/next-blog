089c565529e02c64483092dfc6803952e44389fc
sonukuldeep <sonukuldeep@outlook.com>
2023-02-28 18:01:07 +0530

    updating and minor fix

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import Image from 'next/image'
+import urlFor from '../lib/urlFor'
+import ClientSideRoute from './ClientSideRoute'
 
 interface Props {
-    posts: Post[]
+  posts: Post[]
 }
 
-const BlogList = ({posts}: Props) => {
-    console.log(posts.length)
+const BlogList: React.FC<Props> = ({ posts }) => {
   return (
-    <div>BlogList</div>
+    <>
+      <hr className="color-yellow-500 mb-5" />
+      <div className='grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24'>
+        {posts.map(post => (
+          <ClientSideRoute route={post.slug ? `/post/${post.slug.current}`: (Math.random() * 10000).toString()}>
+            <div key={post._id} className='flex flex-col group cursor-pointer'>
+              <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+                <Image src={post.mainImage ? urlFor(post.mainImage).url(): "/assets/images/placeholder-image.webp"} alt="main image" fill className='object-cover object-left lg:object-center' />
+                <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                  <div>
+                    <p className="font-bold">{post.title}</p>
+                    <p>
+                      {new Date(post._createdAt).toLocaleDateString('en-US', {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </p>
+                  </div>
+                  <div className='flex flex-col md:flex-row gap-y-2'>
+                    {post.categories ? post.categories.map(category => (
+                      <div key={category._id} className='text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
+                        <p>{category.title}</p>
+                      </div>
+                    )) : ""}
+                  </div>
+                </div>
+              </div>
+              <div className='mt-5 flex-1'>
+                <p className='underline text-lg font-bold'>{post.title}</p>
+                <p className='text-gray-500'>{post.description}</p>
+              </div>
+              <p className='mt-5 font-bold flex items-center group-hover:underline'>Read Post</p>
+            </div>
+          </ClientSideRoute>
+        ))}
+      </div>
+    </>
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
   experimental: {
     appDir: true
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/7risvk1b/production/**',
+      },
+    ],
+  },
 }
