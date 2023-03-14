fa2d8ff5e68544d190b3743bea158e1548717f7f
sonukuldeep <sonukuldeep@outlook.com>
2023-03-14 19:29:46 +0530

    embadded update 14 March

--- components/StudioNavbar.tsx
+++ components/StudioNavbar.tsx
@@ -4,10 +4,10 @@ import { BiExit } from 'react-icons/Bi';
 
 const StudioNavbar = (props: any) => {
   return (
-    <div>
+    <div className="bg-[#101112]">
       <div className="flex gap-2">
-        <Link className="flex items-center mx-2 hover:text-white font-semibold capitalize text-gray-300" href="/"><MdOutlineArrowBack size="1.4rem" /><span className="m-3">Go back to website</span></Link>
-        <Link className="flex items-center mx-2 hover:text-white font-semibold capitalize text-gray-300" href="/api/exit-preview"><BiExit size="1.4rem" /><span className="m-3">Exit preview mode</span></Link>
+        <Link className="flex items-center mx-2 hover:text-[rgba(134,144,160,0.4)] font-semibold capitalize text-gray-300" href="/"><MdOutlineArrowBack size="1.4rem" /><span className="m-3">Go back to website</span></Link>
+        <Link className="flex items-center mx-2 hover:text-[rgba(134,144,160,0.4)] font-semibold capitalize text-gray-300" href="/api/exit-preview"><BiExit size="1.4rem" /><span className="m-3">Exit preview mode</span></Link>
       </div>
         {props.renderDefault(props)}
     </div>
